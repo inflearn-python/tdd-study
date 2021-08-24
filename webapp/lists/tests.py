23ec0a38fd07875ea9b3022830cbce786edcd419
jslee <dlwhdtjd098@naver.com>
2021-08-24 22:21:06 +0900

    tdd 3장 - 뷰를 위한 단위 테스트
    - 최소 HTML을 반호나하는 기본 뷰

--- webapp/lists/tests.py
+++ webapp/lists/tests.py
@@ -1,3 +1,4 @@
+from django.http import HttpRequest
 from django.test import TestCase
 from django.urls import resolve
 from lists.views import home_page
@@ -5,8 +6,15 @@ from lists.views import home_page
 
 class SmokeTest(TestCase):
     def test_bad_maths(self):
-        self.assertEqual(1 + 1, 3)
+        self.assertEqual(1 + 1, 2)
 
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>To-Do lists</title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
